670a4a1259e243e7c52726bedb92ac61eacfa8d9
Kai Sells <kai.sells@hotmail.co.uk>
2021-01-31 17:23:30 +0000

    GH pages deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter baseName='/covidata'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
